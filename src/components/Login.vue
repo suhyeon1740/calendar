25a2e0db63830f9a4fbc8686613fd82c60656b2b
suhyeon <suhyeon1740@gmail.com>
2019-02-13 23:40:35 +0900

    first commit

---
+++ src/components/Login.vue
@@ -0,0 +1,24 @@
+<template lang="html">
+  <div id="login">
+      <div class="id">
+          <label for="id">ID</label>
+          <input type="text" name="id" value="">
+      </div>
+      <div class="pw">
+          <label for="pw">PW</label>
+          <input type="text" name="pw" value="">
+      </div>
+      <button type="button" name="button">LOGIN</button>
+      <a href="#">회원가입</a>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: 'login'
+}
+</script>
+
+<style lang="css" scoped>
+</style>
